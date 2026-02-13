f3289f8d9aafaf62984dda83079f804a185bf834
scen1c <pavel.volovoi@gmail.com>
2026-02-13 18:42:01 +0800

    Fixed file account_utils.rs

--- src/account_utils.rs
+++ src/account_utils.rs
@@ -1,6 +1,5 @@
 use std::fs;
-use std::io;
-
+use std::io::{self, Write};
 use crate::BankAccountRust;
 
 pub fn save_account(accounts: &Vec<BankAccountRust>, path: &str) -> io::Result<()> {
@@ -14,3 +13,35 @@ pub fn load_account(path: &str) -> io::Result<Vec<BankAccountRust>> {
     let accounts: Vec<BankAccountRust> = serde_json::from_str(&file_content).unwrap();
     Ok(accounts)
 }
+
+pub fn creating_user() -> BankAccountRust {
+    let mut name = String::new();
+    let mut email = String::new();
+    let mut phone_string = String::new();
+    let mut password = String::new();
+    print!("Lets create ur account. Whats ur name?: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut name).expect("Error");
+    let name = name.trim().to_string();
+    print!("Whats ur email?: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut email).expect("Error");
+    let email = email.trim().to_string();
+    print!("Whats ur phone number?: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut phone_string).expect("Error");
+    let phone = phone_string.trim().parse().expect("Error");
+    print!("Lets write ur password! Create password: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut password).expect("Error");
+    let password = password.trim().to_string();
+    BankAccountRust { 
+        name, 
+        account_id: 1, 
+        balance: 0, 
+        email, 
+        phone, 
+        password,
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn main() {
     let answer = answer.trim();
 
     if answer == "n" {
-        let new_account = creating_user();
+        let new_account = account_utils::creating_user();
         println!("Ur account succesfully created!");
 
         let mut accounts = account_utils::load_account("accounts.json").unwrap_or_else(|_| Vec::new());
@@ -55,34 +55,3 @@ fn main() {
         println!("Please enter only 'y' or 'n'");
     }
 }
-fn creating_user() -> BankAccountRust {
-    let mut name = String::new();
-    let mut email = String::new();
-    let mut phone_string = String::new();
-    let mut password = String::new();
-    print!("Lets create ur account. Whats ur name?: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut name).expect("Error");
-    let name = name.trim().to_string();
-    print!("Whats ur email?: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut email).expect("Error");
-    let email = email.trim().to_string();
-    print!("Whats ur phone number?: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut phone_string).expect("Error");
-    let phone = phone_string.trim().parse().expect("Error");
-    print!("Lets write ur password! Create password: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut password).expect("Error");
-    let password = password.trim().to_string();
-    BankAccountRust { 
-        name, 
-        account_id: 1, 
-        balance: 0, 
-        email, 
-        phone, 
-        password,
-    }
-}
-
